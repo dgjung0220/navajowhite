7b07268e8cee175847915db8a92d463ddf2c6cdc
Donggoo Jung <dgjung0220@gmail.com>
2017-12-20 17:11:16 +0900

    mobile vision with firebase

---
+++ app/src/main/java/com/bearpot/arNaviProto1/OcrDetectorProcessor.java
@@ -0,0 +1,48 @@
+package com.bearpot.arNaviProto1;
+
+/**
+ * Created by dg.jung on 2017-12-20.
+ */
+
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.bearpot.arNaviProto1.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+
+/**
+ * A very simple Processor which gets detected TextBlocks and adds them to the overlay
+ * as OcrGraphics.
+ * TODO: Make this implement Detector.Processor<TextBlock> and add text to the GraphicOverlay
+ */
+public class OcrDetectorProcessor implements Detector.Processor<TextBlock> {
+
+    private GraphicOverlay<OcrGraphic> mGraphicOverlay;
+
+    OcrDetectorProcessor(GraphicOverlay<OcrGraphic> ocrGraphicOverlay) {
+        mGraphicOverlay = ocrGraphicOverlay;
+    }
+
+    // TODO:  Once this implements Detector.Processor<TextBlock>, implement the abstract methods.
+    @Override
+    public void release() {
+        mGraphicOverlay.clear();
+    }
+
+    @Override
+    public void receiveDetections(Detector.Detections<TextBlock> detections) {
+        mGraphicOverlay.clear();
+
+        SparseArray<TextBlock> items = detections.getDetectedItems();
+        for (int i = 0; i < items.size(); ++i) {
+            TextBlock item = items.valueAt(i);
+
+            if (item != null && item.getValue() != null) {
+                Log.d("Processor", "Text detected! " + item.getValue());
+            }
+            OcrGraphic graphic = new OcrGraphic(mGraphicOverlay, item);
+            mGraphicOverlay.add(graphic);
+        }
+    }
+}
---
+++ app/src/main/java/com/bearpot/arNaviProto1/VO/Target.java
@@ -0,0 +1,26 @@
+package com.bearpot.arNaviProto1.VO;
+
+/**
+ * Created by dg.jung on 2017-12-20.
+ */
+
+public class Target {
+    private long createdDate;
+    private String target;
+
+    public long getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(long createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+}
